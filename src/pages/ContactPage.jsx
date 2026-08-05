ad9cdc9900a011f1ef4f9d0ea236f002d55ac747
Amine Nahi <amine.nahi@etu.univ-amu.fr>
2026-08-05 13:45:30 +0200

    Suppression contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,12 +1,10 @@
-import { Helmet } from 'react-helmet-async';
+
 import Contact from '../components/Contact';
 
 export default function ContactPage() {
   return (
     <>
-      <Helmet>
-        <title>Contact | Amine Nahi</title>
-      </Helmet>
+
       
       <div style={{ paddingTop: '80px' }}>
         <Contact />
